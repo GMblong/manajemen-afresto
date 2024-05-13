4f616c4876ad8ebf9d122617a0553af0e69fed02
GMblong <andiarifrahmatullah@gmail.com>
2024-05-13 14:26:56 +0700

    fix:terbilang

--- app/helpers.php
+++ app/helpers.php
@@ -11,6 +11,44 @@ if (!function_exists('formatCurrency')) {
     function formatCurrency($amount, $currency = 'Rp') {
         return $currency . number_format($amount, 0, ',', '.');
     }
+
+    function terbilang($number) {
+        $bilangan = array(
+            '',
+            'satu',
+            'dua',
+            'tiga',
+            'empat',
+            'lima',
+            'enam',
+            'tujuh',
+            'delapan',
+            'sembilan',
+            'sepuluh',
+            'sebelas'
+        );
+    
+        if ($number < 12) {
+            return $bilangan[$number];
+        } elseif ($number < 20) {
+            return $bilangan[$number - 10] . ' belas';
+        } elseif ($number < 100) {
+            return $bilangan[(int) ($number / 10)] . ' puluh ' . $bilangan[$number % 10];
+        } elseif ($number < 200) {
+            return 'seratus ' . terbilang($number - 100);
+        } elseif ($number < 1000) {
+            return $bilangan[(int) ($number / 100)] . ' ratus ' . terbilang($number % 100);
+        } elseif ($number < 2000) {
+            return 'seribu ' . terbilang($number - 1000);
+        } elseif ($number < 1000000) {
+            return terbilang((int) ($number / 1000)) . ' ribu ' . terbilang($number % 1000);
+        } elseif ($number < 1000000000) {
+            return terbilang((int) ($number / 1000000)) . ' juta ' . terbilang($number % 1000000);
+        }
+    
+        return 'undefined';
+    }
+    
 }      
 
 if (!function_exists('getIndonesianMonth')) {
